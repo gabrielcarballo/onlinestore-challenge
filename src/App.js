b8b3a9aa8ff2ea3032770673e1dd37888c86027e
Bernardo Lavinas <bernardorclavinas@gmail.com>
2022-09-08 19:15:17 -0300

    Req 3

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import HomePage from './components/HomePage';
 import Aside from './components/Aside';
 import CartBtn from './components/CartBtn';
+import CartPage from './components/CartPage';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
             <Aside />
             <HomePage />
           </Route>
+          <Route exact path="/CartPage" component={ CartPage } />
         </Switch>
         <CartBtn />
       </div>
--- src/components/CartBtn.jsx
+++ src/components/CartBtn.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 export default class CartBtn extends Component {
   render() {
     return (
-      <div>CartBtn</div>
+      <div>
+        <NavLink to="/CartPage" data-testid="shopping-cart-button">
+          Carrinho de Compras
+        </NavLink>
+      </div>
     );
   }
 }
