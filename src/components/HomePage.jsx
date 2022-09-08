68df1357dca39213e66664f0959fda32c4078433
MarcosKern <vkg.marcos@gmail.com>
2022-09-08 19:53:32 -0300

    Req 05

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,17 +1,93 @@
 import React, { Component } from 'react';
+import { getProductsFromCategoryAndQuery } from '../services/api';
+import ProductCard from './ProductCard';
 
 export default class HomePage extends Component {
   state = {
     haveContent: false,
+    searchResult: true,
+    queryInput: '',
+    productsArray: [],
+  };
+
+  inputHandle = (event) => {
+    const { target: { value } } = event;
+    this.setState({ queryInput: value });
+  };
+
+  buttonHandler = async (event) => {
+    const found = 0;
+    const { queryInput } = this.state;
+    event.preventDefault();
+    const products = await getProductsFromCategoryAndQuery({
+      categoryId: '',
+      query: `${queryInput}`,
+    });
+    const { results } = products;
+    this.setState({ productsArray: results });
+    if (results.length === found) {
+      this.setState({ searchResult: true });
+    } else {
+      this.setState({ searchResult: false });
+    }
+    this.setState({ haveContent: true });
   };
 
   render() {
-    const { haveContent } = this.state;
+    const { haveContent, productsArray, searchResult } = this.state;
     return (
       <div>
+        <form
+          action=""
+        >
+          <input
+            type="text"
+            data-testid="query-input"
+            onChange={ this.inputHandle }
+          />
+          <button
+            type="submit"
+            data-testid="query-button"
+            onClick={ this.buttonHandler }
+          >
+            pesquisar
+          </button>
+        </form>
         {(
           haveContent
-            ? <p>Se quiseres conecer</p>
+            ? (
+              <div>
+                {
+                  searchResult
+                    ? (
+                      <p>
+                        Nenhum produto foi encontrado
+                      </p>
+                    )
+                    : (
+                      <ul>
+                        {
+                          productsArray.map((product) => {
+                            const { title, id, price } = product;
+                            return (
+                              <li
+                                key={ id }
+                                data-testid="product"
+                              >
+                                <ProductCard
+                                  id={ id }
+                                  price={ price }
+                                  title={ title }
+                                />
+                              </li>
+                            );
+                          })
+                        }
+                      </ul>
+                    )
+                }
+              </div>
+            )
             : (
               <h1 data-testid="home-initial-message">
                 Digite algum termo de pesquisa ou escolha uma categoria.
